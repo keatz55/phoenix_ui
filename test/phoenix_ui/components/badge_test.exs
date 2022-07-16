ee85e1027bc88354e195dbbc655963b4d9f89db3
Jace Warren <keatz55@gmail.com>
2022-07-15 19:46:22 -0500

    add basic badge test

---
+++ test/phoenix_ui/components/badge_test.exs
@@ -0,0 +1,29 @@
+defmodule PhoenixUI.Components.BadgeTest do
+  use PhoenixUI.Case, async: true
+
+  setup do
+    [assigns: %{}]
+  end
+
+  describe "badge/1" do
+    test "should render with defaults", %{assigns: assigns} do
+      markup = ~H"""
+      <.badge content="99+">
+        <.heroicon color="slate" name="mail"/>
+      </.badge>
+      """
+
+      html = rendered_to_string(markup)
+
+      assert html =~ "<div class=\"badge-wrapper "
+      assert html =~ "<div class=\"badge "
+      assert html =~ "99+"
+    end
+  end
+
+  describe "classes/0" do
+    test "should generate a list of all possible classes for Tailwind CSS JIT compiler" do
+      assert [_ | _] = PhoenixUI.Components.Badge.classes()
+    end
+  end
+end
