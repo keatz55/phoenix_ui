6e2d4ba9654a14ac20c3e8005df5f4045ccc9f72
Jace Warren <keatz55@gmail.com>
2022-09-24 17:14:23 -0500

    updates list

--- lib/phoenix_ui/components/list.ex
+++ lib/phoenix_ui/components/list.ex
@@ -2,12 +2,9 @@ defmodule PhoenixUI.Components.List do
   @moduledoc """
   Provides list component.
   """
-  import PhoenixUI.Components.Element
-
   use PhoenixUI, :component
 
-  @default_list_element "ul"
-  @default_list_item_element "li"
+  attr(:element, :string, default: "ul")
 
   @doc """
   Renders list component.
@@ -23,18 +20,20 @@ defmodule PhoenixUI.Components.List do
 
   """
   @spec list(Socket.assigns()) :: Rendered.t()
-  def list(raw_assigns) do
+  def list(prev_assigns) do
     assigns =
-      raw_assigns
-      |> assign_new(:element, fn -> @default_list_element end)
-      |> build_list_attrs()
+      prev_assigns
+      |> assign_class(~w(py-1))
+      |> assign_rest()
 
     ~H"""
-    <.element {@list_attrs}>
+    <.dynamic_tag name={@element} class={@class} {@rest}>
       <%= render_slot(@inner_block) %>
-    </.element>
+    </.dynamic_tag>
     """
   end
+
+  attr(:element, :string, default: "li")
 
   @doc """
   Renders list item component.
@@ -50,16 +49,19 @@ defmodule PhoenixUI.Components.List do
 
   """
   @spec list_item(Socket.assigns()) :: Rendered.t()
-  def list_item(raw_assigns) do
+  def list_item(prev_assigns) do
     assigns =
-      raw_assigns
-      |> assign_new(:element, fn -> @default_list_item_element end)
-      |> build_list_item_attrs()
+      prev_assigns
+      |> assign_class(~w(
+        block w-full whitespace-nowrap py-2 px-4 text-sm font-normal
+        text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-100/25
+      ))
+      |> assign_rest()
 
     ~H"""
-    <.element {@list_item_attrs}>
+    <.dynamic_tag name={@element} class={@class} {@rest}>
       <%= render_slot(@inner_block) %>
-    </.element>
+    </.dynamic_tag>
     """
   end
 
@@ -80,40 +82,5 @@ defmodule PhoenixUI.Components.List do
     ]
     |> List.flatten()
     |> Enum.uniq()
-  end
-
-  ### List Attrs ##########################
-
-  defp build_list_attrs(assigns) do
-    extend_class = build_class(~w(
-      py-1
-      #{Map.get(assigns, :extend_class)}
-    ))
-
-    attrs =
-      assigns
-      |> assigns_to_attributes([:extend_class])
-      |> Keyword.put(:variant, assigns[:element])
-      |> Keyword.put(:extend_class, extend_class)
-
-    assign(assigns, :list_attrs, attrs)
-  end
-
-  ### List Item Attrs ##########################
-
-  defp build_list_item_attrs(assigns) do
-    class = build_class(~w(
-      block w-full whitespace-nowrap py-2 px-4 text-sm font-normal
-      text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-100/25
-      #{Map.get(assigns, :extend_class)}
-    ))
-
-    attrs =
-      assigns
-      |> assigns_to_attributes([:element, :extend_class])
-      |> Keyword.put(:variant, assigns[:element])
-      |> Keyword.put_new(:extend_class, class)
-
-    assign(assigns, :list_item_attrs, attrs)
   end
 end
--- lib/phoenix_ui/helpers.ex
+++ lib/phoenix_ui/helpers.ex
@@ -4,7 +4,44 @@ defmodule PhoenixUI.Helpers do
   """
   alias Phoenix.{HTML.Form, LiveView.Socket}
 
-  import Phoenix.LiveViewTest
+  import Phoenix.{Component, LiveViewTest}
+
+  @doc """
+  Builds and normalizes list of classes.
+
+  ## Examples
+
+      iex> assign_class(assigns, [" class1", "class2 ", ...])
+      %{class: "class1 class2 ...", ..}
+
+  """
+  @spec assign_class(Socket.assigns(), [String.t()]) :: Socket.assigns()
+  def assign_class(assigns, []), do: assigns
+
+  def assign_class(assigns, class_list) do
+    assign_new(assigns, :class, fn ->
+      if extend_class = Map.get(assigns, :extend_class) do
+        [extend_class | class_list] |> Enum.reverse() |> Enum.join(" ") |> String.trim()
+      else
+        class_list |> Enum.join(" ") |> String.trim()
+      end
+    end)
+  end
+
+  @doc """
+  Builds and normalizes list of classes.
+
+  ## Examples
+
+      iex> assign_rest(assigns, exclude)
+      %{...}
+
+  """
+  @spec assign_rest(Socket.assigns()) :: Socket.assigns()
+  @spec assign_rest(Socket.assigns(), []) :: Socket.assigns()
+  def assign_rest(assigns, exclude \\ []) do
+    assign(assigns, :rest, assigns_to_attributes(assigns, exclude))
+  end
 
   @doc """
   Builds and normalizes list of classes.
