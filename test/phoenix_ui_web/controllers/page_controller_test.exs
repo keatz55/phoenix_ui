528f6d0972342c22b7e874065e6a06c87ea4259c
Jace Warren <keatz55@gmail.com>
2024-11-29 17:39:20 -0600

    fix init test

--- test/phoenix_ui_web/controllers/page_controller_test.exs
+++ test/phoenix_ui_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoenixUIWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert redirected_to(conn) == "/getting_started"
   end
 end
